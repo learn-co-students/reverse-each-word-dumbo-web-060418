4f52ae50498a7c02deedc7f80a8fdef0e9c2ca37
michermd <michermd@gmail.com>
2018-05-30 12:34:56 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
+# def reverse_each_word(string)
+#   split_strings = string.split()
+#   split_strings.collect { |word| word.reverse  }.join(' ')
+# end
+
 def reverse_each_word(string)
-  split_strings = string.split()
-  split_strings.collect { |word| word.reverse  }.join(' ')
+  string.split().collect { |word| word.reverse  }.join(' ')
 end
